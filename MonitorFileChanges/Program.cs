b2a2bc4dcd50c7bd1ce218a97a005383f55afa65
Cabe <cabe@rocketmail.com>
2018-08-17 22:16:33 -0600

    Basic structure. We'll fill in the gaps next.

--- MonitorFileChanges/Program.cs
+++ MonitorFileChanges/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MonitorFileChanges.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,35 @@ using System.Threading.Tasks;
 
 namespace MonitorFileChanges
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+
         }
+        private static void RulesCheck(string directory, string pattern)
+        {
+
+        }
+
+        public static FileChangeReqs GetFileChangeReqs(string directory, string pattern)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ParseEngine(FileChangeReqs reqs) { }
+
+        private static void Parse(ref FileChangeReqs reqsBefore) { }
+
+        private static int GetFileLinesCount(string file)
+        {
+            throw new NotImplementedException();
+        }
+        private static DateTime GetFileDateModified(string file)
+        {
+            throw new NotImplementedException();
+
+        }
+
     }
 }
